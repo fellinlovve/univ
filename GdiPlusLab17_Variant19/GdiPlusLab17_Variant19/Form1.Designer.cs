b927697c99d3dc5e69fcfd2999f27d8ce973edaf
fellinlovve <89064926205a@gmail.com>
2025-12-29 16:23:36 +0300

    Add files via upload

---
+++ GdiPlusLab17_Variant19/GdiPlusLab17_Variant19/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace GdiPlusLab17_Variant19
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.drawingPanel = new System.Windows.Forms.Panel();
+            this.btnSelectColor = new System.Windows.Forms.Button();
+            this.btnStartStopAnimation = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.animationTimer = new System.Windows.Forms.Timer(this.components);
+            this.lblCircle1 = new System.Windows.Forms.Label();
+            this.lblCircle2 = new System.Windows.Forms.Label();
+            this.lblTopArea = new System.Windows.Forms.Label();
+            this.lblBottomArea = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+
+            // drawingPanel
+            this.drawingPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.drawingPanel.Location = new System.Drawing.Point(0, 0);
+            this.drawingPanel.Name = "drawingPanel";
+            this.drawingPanel.Size = new System.Drawing.Size(800, 450);
+            this.drawingPanel.TabIndex = 0;
+            this.drawingPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.drawingPanel_Paint);
+            this.drawingPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.drawingPanel_MouseMove);
+
+            // btnSelectColor
+            this.btnSelectColor.Location = new System.Drawing.Point(12, 12);
+            this.btnSelectColor.Name = "btnSelectColor";
+            this.btnSelectColor.Size = new System.Drawing.Size(120, 23);
+            this.btnSelectColor.TabIndex = 1;
+            this.btnSelectColor.Text = "Выбрать цвет";
+            this.btnSelectColor.UseVisualStyleBackColor = true;
+            this.btnSelectColor.Click += new System.EventHandler(this.btnSelectColor_Click);
+
+            // btnStartStopAnimation
+            this.btnStartStopAnimation.Location = new System.Drawing.Point(138, 12);
+            this.btnStartStopAnimation.Name = "btnStartStopAnimation";
+            this.btnStartStopAnimation.Size = new System.Drawing.Size(120, 23);
+            this.btnStartStopAnimation.TabIndex = 2;
+            this.btnStartStopAnimation.Text = "Запуск анимации";
+            this.btnStartStopAnimation.UseVisualStyleBackColor = true;
+            this.btnStartStopAnimation.Click += new System.EventHandler(this.btnStartStopAnimation_Click);
+
+            // animationTimer
+            this.animationTimer.Interval = 100;
+            this.animationTimer.Tick += new System.EventHandler(this.animationTimer_Tick);
+
+            // lblCircle1
+            this.lblCircle1.AutoSize = true;
+            this.lblCircle1.BackColor = System.Drawing.Color.Transparent;
+            this.lblCircle1.Location = new System.Drawing.Point(100, 200);
+            this.lblCircle1.Name = "lblCircle1";
+            this.lblCircle1.Size = new System.Drawing.Size(52, 15);
+            this.lblCircle1.Text = "Круг 1";
+            this.lblCircle1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label_MouseDown);
+            this.lblCircle1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.label_MouseMove);
+            this.lblCircle1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.label_MouseUp);
+
+            // lblCircle2
+            this.lblCircle2.AutoSize = true;
+            this.lblCircle2.BackColor = System.Drawing.Color.Transparent;
+            this.lblCircle2.Location = new System.Drawing.Point(300, 200);
+            this.lblCircle2.Name = "lblCircle2";
+            this.lblCircle2.Size = new System.Drawing.Size(52, 15);
+            this.lblCircle2.Text = "Круг 2";
+            this.lblCircle2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label_MouseDown);
+            this.lblCircle2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.label_MouseMove);
+            this.lblCircle2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.label_MouseUp);
+
+            // lblTopArea
+            this.lblTopArea.AutoSize = true;
+            this.lblTopArea.BackColor = System.Drawing.Color.Transparent;
+            this.lblTopArea.Location = new System.Drawing.Point(200, 100);
+            this.lblTopArea.Name = "lblTopArea";
+            this.lblTopArea.Size = new System.Drawing.Size(64, 15);
+            this.lblTopArea.Text = "Верхняя";
+            this.lblTopArea.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label_MouseDown);
+            this.lblTopArea.MouseMove += new System.Windows.Forms.MouseEventHandler(this.label_MouseMove);
+            this.lblTopArea.MouseUp += new System.Windows.Forms.MouseEventHandler(this.label_MouseUp);
+
+            // lblBottomArea
+            this.lblBottomArea.AutoSize = true;
+            this.lblBottomArea.BackColor = System.Drawing.Color.Transparent;
+            this.lblBottomArea.Location = new System.Drawing.Point(200, 300);
+            this.lblBottomArea.Name = "lblBottomArea";
+            this.lblBottomArea.Size = new System.Drawing.Size(65, 15);
+            this.lblBottomArea.Text = "Нижняя";
+            this.lblBottomArea.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label_MouseDown);
+            this.lblBottomArea.MouseMove += new System.Windows.Forms.MouseEventHandler(this.label_MouseMove);
+            this.lblBottomArea.MouseUp += new System.Windows.Forms.MouseEventHandler(this.label_MouseUp);
+
+            // Form1
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblBottomArea);
+            this.Controls.Add(this.lblTopArea);
+            this.Controls.Add(this.lblCircle2);
+            this.Controls.Add(this.lblCircle1);
+            this.Controls.Add(this.btnStartStopAnimation);
+            this.Controls.Add(this.btnSelectColor);
+            this.Controls.Add(this.drawingPanel);
+            this.Name = "Form1";
+            this.Text = "Лабораторная работа 17 — Вариант 19";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel drawingPanel;
+        private System.Windows.Forms.Button btnSelectColor;
+        private System.Windows.Forms.Button btnStartStopAnimation;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Timer animationTimer;
+        private System.Windows.Forms.Label lblCircle1;
+        private System.Windows.Forms.Label lblCircle2;
+        private System.Windows.Forms.Label lblTopArea;
+        private System.Windows.Forms.Label lblBottomArea;
+    }
+}
---
+++ GdiPlusLab17_Variant19/GdiPlusLab17_Variant19/Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace GdiPlusLab17_Variant19
+{
+    public partial class Form1 : Form
+    {
+        // Графические пути для проверки попадания мыши
+        private GraphicsPath circle1Path;
+        private GraphicsPath circle2Path;
+        private GraphicsPath topAreaPath;
+        private GraphicsPath bottomAreaPath;
+
+        // Цвета для закрашенных областей
+        private Color circle1Color = Color.LightBlue;
+        private Color circle2Color = Color.LightGreen;
+        private Color topAreaColor = Color.Red;
+        private Color bottomAreaColor = Color.Blue;
+
+        // Пользовательский цвет
+        private Color userSelectedColor = Color.Yellow;
+
+        // Анимация
+        private bool isAnimating = false;
+        private float fontSize = 10f;
+        private float fontSizeDirection = 0.3f;
+        private int hue = 0;
+
+        // Перетаскивание меток
+        private bool isDragging = false;
+        private Label draggingLabel;
+        private Point dragOffset;
+
+        public Form1()
+        {
+            InitializeComponent();
+            SetupPaths();
+        }
+
+        private void SetupPaths()
+        {
+            // Круг 1: центр (0,0), радиус 2 -> координаты на панели: (200, 200), радиус 100
+            circle1Path = new GraphicsPath();
+            circle1Path.AddEllipse(100, 100, 200, 200); // x=100, y=100, ширина=200, высота=200
+
+            // Круг 2: центр (2,0), радиус 2 -> координаты на панели: (300, 200), радиус 100
+            circle2Path = new GraphicsPath();
+            circle2Path.AddEllipse(200, 100, 200, 200); // x=200, y=100, ширина=200, высота=200
+
+            // Верхняя область: прямоугольник от x=0 до x=4, y=2 до y=3 -> координаты на панели: x=0 до x=400, y=100 до y=150
+            topAreaPath = new GraphicsPath();
+            topAreaPath.AddRectangle(new Rectangle(0, 100, 400, 50));
+
+            // Нижняя область: прямоугольник от x=0 до x=2, y=-2 до y=0 -> координаты на панели: x=0 до x=200, y=200 до y=300
+            bottomAreaPath = new GraphicsPath();
+            bottomAreaPath.AddRectangle(new Rectangle(0, 200, 200, 100));
+        }
+
+        private void drawingPanel_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+
+            // Оси координат
+            Pen axisPen = new Pen(Color.Gray, 2);
+            g.DrawLine(axisPen, 0, 200, 800, 200); // ось X
+            g.DrawLine(axisPen, 200, 0, 200, 450); // ось Y
+
+            // Рисуем круги
+            g.DrawPath(Pens.Black, circle1Path);
+            g.DrawPath(Pens.Black, circle2Path);
+
+            // Закрашиваем области
+            g.FillPath(new SolidBrush(circle1Color), circle1Path);
+            g.FillPath(new SolidBrush(circle2Color), circle2Path);
+            g.FillPath(new SolidBrush(topAreaColor), topAreaPath);
+            g.FillPath(new SolidBrush(bottomAreaColor), bottomAreaPath);
+
+            // Рисуем линии границ (по условию: горизонтальные линии y=2, y=3 и вертикальные x=2, x=4)
+            Pen borderPen = new Pen(Color.Gray, 1);
+            g.DrawLine(borderPen, 0, 100, 800, 100); // y=2 (на панели y=100)
+            g.DrawLine(borderPen, 0, 150, 800, 150); // y=3 (на панели y=150)
+            g.DrawLine(borderPen, 200, 0, 200, 450); // x=2 (на панели x=200)
+            g.DrawLine(borderPen, 400, 0, 400, 450); // x=4 (на панели x=400)
+
+            // Подписи осей (не требуется по заданию, но добавим для наглядности)
+            using (Font font = new Font("Arial", 8))
+            {
+                g.DrawString("0", font, Brushes.Black, 190, 205);
+                g.DrawString("2", font, Brushes.Black, 190, 105);
+                g.DrawString("3", font, Brushes.Black, 190, 55);
+                g.DrawString("-2", font, Brushes.Black, 180, 255);
+                g.DrawString("2", font, Brushes.Black, 205, 205);
+                g.DrawString("4", font, Brushes.Black, 405, 205);
+            }
+        }
+
+        private void drawingPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            Point pt = e.Location;
+
+            // Цвета по умолчанию
+            Color defaultCircle1 = Color.LightBlue;
+            Color defaultCircle2 = Color.LightGreen;
+            Color defaultTop = Color.Red;
+            Color defaultBottom = Color.Blue;
+
+            // Изменяем цвет при наведении
+            circle1Color = circle1Path.IsVisible(pt) ? Color.Cyan : defaultCircle1;
+            circle2Color = circle2Path.IsVisible(pt) ? Color.Lime : defaultCircle2;
+            topAreaColor = topAreaPath.IsVisible(pt) ? Color.Magenta : defaultTop;
+            bottomAreaColor = bottomAreaPath.IsVisible(pt) ? Color.Cyan : defaultBottom;
+
+            drawingPanel.Invalidate();
+        }
+
+        private void btnSelectColor_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                userSelectedColor = colorDialog1.Color;
+                // Применяем выбранный цвет к одной из областей (например, к нижней)
+                bottomAreaColor = userSelectedColor;
+                drawingPanel.Invalidate();
+            }
+        }
+
+        private void btnStartStopAnimation_Click(object sender, EventArgs e)
+        {
+            isAnimating = !isAnimating;
+            btnStartStopAnimation.Text = isAnimating ? "Остановить анимацию" : "Запуск анимации";
+            if (isAnimating)
+                animationTimer.Start();
+            else
+                animationTimer.Stop();
+        }
+
+        private void animationTimer_Tick(object sender, EventArgs e)
+        {
+            // Анимация размера шрифта подписей
+            fontSize += fontSizeDirection;
+            if (fontSize >= 16 || fontSize <= 8)
+                fontSizeDirection = -fontSizeDirection;
+
+            lblCircle1.Font = new Font("Arial", fontSize);
+            lblCircle2.Font = new Font("Arial", fontSize);
+            lblTopArea.Font = new Font("Arial", fontSize);
+            lblBottomArea.Font = new Font("Arial", fontSize);
+
+            // Анимация цвета верхней области через HSV
+            hue = (hue + 2) % 360;
+            topAreaColor = ColorFromHsv(hue, 1.0f, 1.0f);
+            drawingPanel.Invalidate();
+        }
+
+        // Преобразование HSV → RGB
+        public static Color ColorFromHsv(float h, float s, float v)
+        {
+            int hi = (int)Math.Floor(h / 60) % 6;
+            float f = h / 60 - (float)Math.Floor(h / 60);
+            float p = v * (1 - s);
+            float q = v * (1 - f * s);
+            float t = v * (1 - (1 - f) * s);
+            switch (hi)
+            {
+                case 0:
+                    return Color.FromArgb((int)(v * 255), (int)(t * 255), (int)(p * 255));
+                case 1:
+                    return Color.FromArgb((int)(q * 255), (int)(v * 255), (int)(p * 255));
+                case 2:
+                    return Color.FromArgb((int)(p * 255), (int)(v * 255), (int)(t * 255));
+                case 3:
+                    return Color.FromArgb((int)(p * 255), (int)(q * 255), (int)(v * 255));
+                case 4:
+                    return Color.FromArgb((int)(t * 255), (int)(p * 255), (int)(v * 255));
+                default:
+                    return Color.FromArgb((int)(v * 255), (int)(p * 255), (int)(q * 255));
+            }
+        }
+
+        // Перетаскивание меток
+        private void label_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                draggingLabel = (Label)sender;
+                dragOffset = new Point(e.X, e.Y);
+                isDragging = true;
+            }
+        }
+
+        private void label_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDragging && draggingLabel != null)
+            {
+                Point newLocation = drawingPanel.PointToClient(Cursor.Position);
+                newLocation.Offset(-dragOffset.X, -dragOffset.Y);
+                draggingLabel.Location = newLocation;
+            }
+        }
+
+        private void label_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDragging = false;
+            draggingLabel = null;
+        }
+    }
+}
