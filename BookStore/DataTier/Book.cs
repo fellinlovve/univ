c4dcb2a618d92f724d3121640e7acb01b8df0846
fellinlovve <89064926205a@gmail.com>
2026-03-03 12:47:30 +0300

    Delete BookStore directory

--- BookStore/DataTier/Book.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DataTier
-{
-    public class Book
-    {
-        public string Title { get; set; }
-        public string Genre { get; set; }
-        public double Price { get; set; }
-    }
-}
